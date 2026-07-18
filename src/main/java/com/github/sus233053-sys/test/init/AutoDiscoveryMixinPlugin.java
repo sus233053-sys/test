932f4a7047237abd12d0ebb990c788733793635b
GitHub Action <action@github.com>
2026-07-18 19:21:06 +0000

    Initialize template repository proper
    Set package name to com.github.sus233053-sys.test
    Set project name to test
    Set modid to test

--- src/main/java/com/github/sus233053-sys/test/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/sus233053-sys/test/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.sus233053-sys.test.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
--- src/main/java/com/github/sus233053-sys/test/mixin/MixinGuiMainMenu.java
+++ src/main/java/com/github/sus233053-sys/test/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.sus233053-sys.test.mixin;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
